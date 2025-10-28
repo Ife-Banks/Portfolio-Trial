3231377418e063653e6b1c5cdfef3f1f100ae165
Bankole Ifeoluwa <ifeoluwa.bankole05@gmail.com>
2025-10-28 13:43:27 +0100

    select-none added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home'
 function App() {
 
   return (
-    <div className='overflow-hidden'>
+    <div className='overflow-hidden select-none'>
       <Home />
     </div>
   )
